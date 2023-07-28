ec60c4a94afa55366a0749478a799a72a539bb79
tomasbalvocius <tomasbalv@gmail.com>
2023-07-28 19:18:50 +0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container } from "@material-ui/core";
 import "./App.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -9,9 +10,18 @@ import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div>
+    <Container
+      style={{
+        backgroundColor: "#f9f9f9",
+        marginTop: "70px",
+        height: "730px",
+        width: "1300px",
+        borderRadius: "20px",
+        padding: "0px",
+      }}
+    >
       <Sidebar />
-    </div>
+    </Container>
   );
 }
 
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,79 +1,103 @@
-import React, { useState } from "react";
-import {
-  Switch,
-  FormGroup,
-  FormControlLabel,
-  createMuiTheme,
-  ThemeProvider,
-} from "@material-ui/core";
-import NightsStayIcon from "@material-ui/icons/NightsStay";
-import WbSunnyIcon from "@material-ui/icons/WbSunny";
+import React from "react";
+import { Switch } from "@material-ui/core";
+import NightlightRoundOutlinedIcon from "@mui/icons-material/NightlightRoundOutlined";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import Inventory2Icon from "@mui/icons-material/Inventory2";
+import StoreIcon from "@mui/icons-material/Store";
+import SupervisedUserCircleIcon from "@mui/icons-material/SupervisedUserCircle";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import HeadsetMicIcon from "@mui/icons-material/HeadsetMic";
+import SettingsIcon from "@mui/icons-material/Settings";
+import logo from "./images/logo.png";
+import { Badge, withStyles } from "@material-ui/core";
 import "./Sidebar.css";
 
-const theme = createMuiTheme({
-  palette: {
-    secondary: {
-      main: "#ffffff", // white for both modes
-    },
+const StyledBadge = withStyles((theme) => ({
+  badge: {
+    backgroundColor: "#ff0000", // Raudona spalva
+    color: "#fff",
+    height: "20px", // Padidinti taško dydį
+    width: "20px", // Padidinti taško dydį
+    borderRadius: "50%", // Padaryti tašką apvalų
+    display: "flex", // Centruoti skaičių taške
+    justifyContent: "center",
+    alignItems: "center",
   },
-});
+}))(Badge);
 
 const Sidebar = () => {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleDarkMode = (event) => setDarkMode(event.target.checked);
-
-  const sidebarStyle = {
-    backgroundColor: "#D3D3D3", // light grey
-    height: "100vh",
-    width: "200px",
-    padding: "20px",
-  };
-
-  const darkSidebarStyle = {
-    ...sidebarStyle,
-    backgroundColor: "#696969",
-    color: "#ffffff", // dark grey for dark mode
-  };
-
   return (
-    <ThemeProvider theme={theme}>
-      <div
-        className="sidebar"
-        style={darkMode ? darkSidebarStyle : sidebarStyle}
-      >
-        <ul>
-          <li className={darkMode ? "dark-mode" : "light-mode"}>Dashboard</li>
-          <li className={darkMode ? "dark-mode" : "light-mode"}>Product</li>
-          <li className={darkMode ? "dark-mode" : "light-mode"}>Store</li>
-          <li className={darkMode ? "dark-mode" : "light-mode"}>Visitor</li>
-          <li className={darkMode ? "dark-mode" : "light-mode"}>Analytics</li>
-          <li className={darkMode ? "dark-mode" : "light-mode"}>
-            Notification
-          </li>
-          <li className={darkMode ? "dark-mode" : "light-mode"}>Help Center</li>
-          <li className={darkMode ? "dark-mode" : "light-mode"}>Settings</li>
-        </ul>
-        <FormGroup row>
-          {darkMode ? <NightsStayIcon /> : <WbSunnyIcon />}
-          <FormControlLabel
-            control={
-              <Switch
-                checked={darkMode}
-                onChange={toggleDarkMode}
-                color="secondary"
-              />
-            }
-            label="Dark Mode"
-            style={{
-              marginLeft: 10,
-              display: "flex",
-              flexDirection: "row-reverse",
-            }}
-          />
-        </FormGroup>
+    <div className="sidebar">
+      <div className="logo">
+        <img src={logo} alt="princie logo" />
+        <span className="princie_position">Princie</span>
       </div>
-    </ThemeProvider>
+
+      <ul>
+        <li className="light-mode" style={{ backgroundColor: "#000000" }}>
+          <DashboardIcon fontSize="small" color="primary" />
+          <span className="words_position">Dashboard</span>
+        </li>
+        <li className="light-mode">
+          <Inventory2Icon fontSize="small" />
+          <span className="words_position">Product</span>
+          <StyledBadge
+            overlap="circular"
+            variant="standard"
+            badgeContent={2}
+            style={{ marginLeft: "20px" }}
+          />
+        </li>
+        <li className="light-mode">
+          <StoreIcon fontSize="small" />
+          <span className="words_position">Store</span>
+        </li>
+        <li className="light-mode">
+          <SupervisedUserCircleIcon fontSize="small" />
+          <span className="words_position">Visitor</span>
+        </li>
+        <li className="light-mode">
+          <BarChartIcon fontSize="small" />
+          <span className="words_position">Analytics</span>
+        </li>
+        <li className="light-mode">
+          <NotificationsNoneIcon fontSize="small" />
+          <span className="words_position">Notification </span>
+          <StyledBadge
+            overlap="circular"
+            variant="standard"
+            badgeContent={11}
+            style={{ marginLeft: "20px" }}
+          />
+        </li>
+      </ul>
+      <div className="divider"></div>
+
+      <ul>
+        <li className="light-mode">
+          <HeadsetMicIcon fontSize="small" />
+          <span className="words_position">Help Center</span>
+        </li>
+        <li className="light-mode">
+          <SettingsIcon fontSize="small" />
+          <span className="words_position">Settings</span>
+        </li>
+      </ul>
+
+      <ul className="toggle_container">
+        <div className="divider"></div>
+        <div className="toggle_position">
+          <div className="icon_position">
+            <NightlightRoundOutlinedIcon fontSize="small" />
+          </div>
+          <div className="words_position">DarkMode</div>
+          <div className="switch_position">
+            <Switch defaultChecked size="small" color="primary" />
+          </div>
+        </div>
+      </ul>
+    </div>
   );
 };
 
